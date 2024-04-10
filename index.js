96b31914555b3bb0db48721d80670be5578a82e6
siry siu <144174351+sirysiu@users.noreply.github.com>
2024-04-09 21:31:59 -0400

    added new questions and add a file to generate markdown

--- index.js
+++ index.js
@@ -1,105 +1,96 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./generateMarkdown');
+
 
 // const prompt = inquirer.createPromptModule();
 // TODO: Create an array of questions for user input
-const questions = inquirer.createPromptModule();
-questions ([
-    {
-        type: 'input',
-        message: 'Please enter your project name',
-        name: 'project',
-      
-      },
-      {
-        type: 'input',
-        message: 'Please enter a description',
-        name: 'description',
-       
-      },
-      {
-        type: 'input',
-        message: 'Please enter a table of content',
-        name: 'content',
-        
-      },
-      {
-        type: 'input',
-        message: 'List your installations',
-        name: 'installations',
-        
-      },
-      {
-        type: 'input',
-        message: 'List your usage',
-        name: 'usage',
-       
-      },
-      {
-        type: 'input',
-        message: 'List your license',
-        name: 'license',
-        
-      },
-      {
-        type: 'input',
-        message: 'List your contributing',
-        name: 'contributes',
-        
-      },
-      {
-        type: 'input',
-        message: 'List your test instructions ',
-        name: 'test',
-       
-      },
+const questions = [
+  {
+      type: "input",
+      name: "title",
+      message: "What is your project title?",
+  },
+  {
+      type: "input",
+      name: "description",
+      message: "Write your projects description: ",
+  },
+  {
+      type: "input",
+      name: "instalation",
+      message: "Write your projects instalation instructions: ",
+  },
+  {
+      type: "input",
+      name: "usage",
+      message: "Write your projects usage information: ",
+  },
+  {
+      type: "input",
+      name: "screenshot",
+      message: "What is the file name of the screenshot in your assets/images folder? (example: screenshot.png) ",
+  },
+  {
+      type: "input",
+      name: "credits",
+      message: "Write your projects contributing guidelines: ",
+  },
+  {
+      type: "input",
+      name: "test",
+      message: "Write your projects test instructions: ",
+  },
+  {
+      type: "list",
+      name: "license",
+      message: "Select a license to use: ",
+      choices: ["MIT", "Apache", "GNU General Public", "ISC", new inquirer.Separator(), "NONE"]
+  },
+  // TODO: Add questions about github and email for the "Questions" section. 
+  {
+      type: "input",
+      name: "gitHub",
+      message: "Provide your GitHub user name: ",
+  },
+  {
+      type: "input",
+      name: "email",
+      message: "Provide your E-Mail: ",
+  },
+];
 
-])
+/// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  const markdownContent = generateMarkdown(data);
+  fs.writeFile(
+      fileName,
+      markdownContent,
+      (err) => {
+          if (err) {
+              console.error(err);
+              return;
+          }
+          console.log("README.md Created!")
 
-// TODO: Create a function to write README file
-// function writeToFile(file, data) {
-    
-// }
+      }
+  );
+}
 
-.then((data) => {
-    console.log(data);
-    const readMe = `# readme-generator
-    ${data.project}
 
-    Description
-    ${data.description}
-    Table of Contents
-    ${data.content}
-    Installation
-    ${data.installation}
-    Usage
-    ${data.usage}
-    License
-    ${data.license}
-    Contributing
-    ${data.contributes}
-    Test and Questions
-    ${data.test} `
 
-    if (!fs.existsSync('./README.md')) {
-      fs.mkdirSync('./README.md');
-    }
-
-    fs.writeFileSync('README.md', readMe);
-  });
-
-// TODO: Create a function to initialize app
+//TODO: Create a function to initialize app
 function init() {
-  // inquirer
-  // .prompt(questions)
-  // .then((response) => {
-  //     const fileName = "README.md";
-  //     writeToFile(fileName, response)
-  // }
-  // );
+  inquirer
+  .prompt(questions)
+  .then((response) => {
+      const fileName = "README.md";
+      writeToFile(fileName, response)
+  }
+  );
 
 }
 
-// Function call to initialize app
+//Function call to initialize app
 init();
